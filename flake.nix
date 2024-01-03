ac7d1bdeffee95b7f2558d50a0d6d899934e69a2
velllu <91963404+velllu@users.noreply.github.com>
2024-01-03 22:49:25 +0100

    Nuxt rewrite initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.nodejs_20
+      ];
+    };
+  };
+}
